e0e400bfff45ccc8767283eb876add1b2b306768
Eros Pereira <per18044@byui.edu>
2024-01-24 11:54:22 -0700

    I finished the prepare 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // It will test the constructors
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        // Display fractions using GetFractionString method
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction3.GetFractionString());
+
+        // Ity will display decimal values using GetDecimalValue method
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        // It will test getters and setters
+        fraction1.SetTop(3);
+        fraction1.SetBottom(4);
+
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction1.GetBottom());
+
+        // It will display the fraction using GetFractionString after modifying values
+        Console.WriteLine(fraction1.GetFractionString());
     }
 }
---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,56 @@
+public class Fraction
+{
+    private int top;
+    private int bottom;
+
+    // These are the constructors
+    public Fraction()
+    {
+        top = 1;
+        bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        this.top = top;
+        bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        this.top = top;
+        this.bottom = bottom != 0 ? bottom : 1;
+    }
+
+    // These are the getters and Setters
+    public int GetTop()
+    {
+        return top;
+    }
+
+    public void SetTop(int top)
+    {
+        this.top = top;
+    }
+
+    public int GetBottom()
+    {
+        return bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        this.bottom = bottom != 0 ? bottom : 1;
+    }
+
+    // These are the methods to return representations
+    public string GetFractionString()
+    {
+        return $"{top}/{bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)top / bottom;
+    }
+}
