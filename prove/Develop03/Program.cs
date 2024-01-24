266e4f558d09fadfed45f4fa068a60f86b5a37f2
Eros Pereira <per18044@byui.edu>
2024-01-24 10:50:04 -0700

    Finished Develop 03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // The test the constructors
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        // It willdisplay fractions using GetFractionString method
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction3.GetFractionString());
+
+        // It will display decimal values using GetDecimalValue method
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        //The test getters and setters
+        fraction1.SetTop(3);
+        fraction1.SetBottom(4);
+
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction1.GetBottom());
+
+        // It will display the fraction using GetFractionString after modifying values
+        Console.WriteLine(fraction1.GetFractionString());
     }
 }
