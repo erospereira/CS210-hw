130461d639f7ade5d0231bea2e224172f51c5f0c
Eros Pereira <per18044@byui.edu>
2024-02-14 00:32:01 -0700

    I finished Youtube program

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,14 @@
+// Comment.cs
+using System;
+
+public class Comment
+{
+    public string CommenterName { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string commenterName, string text)
+    {
+        CommenterName = commenterName;
+        Text = text;
+    }
+}
--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,30 @@
+// Program.cs
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create videos
+        Video video1 = new Video("Video 1 Title", "Author 1", 120);
+        video1.AddComment("Commenter 1", "This is a comment.");
+        video1.AddComment("Commenter 2", "Another comment here.");
+
+        Video video2 = new Video("Video 2 Title", "Author 2", 180);
+        video2.AddComment("Commenter 3", "Comment on video 2.");
+        video2.AddComment("Commenter 1", "Another comment on video 2.");
+
+        // Create a list of videos
+        List<Video> videos = new List<Video>();
+        videos.Add(video1);
+        videos.Add(video2);
+
+        // Display video details
+        foreach (var video in videos)
+        {
+            video.DisplayDetails();
+            Console.WriteLine();
+        }
     }
 }
